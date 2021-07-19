82f26520c7ca79acb3fdcccb08896a9ed3d686f0
CrissRO <cezarmocanu2014@gmail.com>
2021-07-19 15:58:24 +0300

    [BE0003] I added the greet function

--- management-backend/src/main/java/com/home/managementbackend/controllers/HomeController.java
+++ management-backend/src/main/java/com/home/managementbackend/controllers/HomeController.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+    @GetMapping("/greet")
+    public String greetUser(){
+        return "Hello user! Welcome to management platform!";
+        
+    }
 
     @GetMapping("/hello")
     public String sayHello(){

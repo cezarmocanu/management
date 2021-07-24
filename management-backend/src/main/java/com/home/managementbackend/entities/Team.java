497dcd733503fb74f25b2f7324d06586233ce9be
malexa21 <aaliroalexandra@gmail.com>
2021-07-24 12:31:05 +0300

    [BE0008] added constructor with parameters

---
+++ management-backend/src/main/java/com/home/managementbackend/entities/Team.java
@@ -0,0 +1,16 @@
+package com.home.managementbackend.entities;
+
+public class Team {
+
+public int numberProperty;
+public String textProperty;
+public String departmentProperty; 
+
+public Team(int number, String text,String department) {
+    numberProperty = number;
+    textProperty = text;
+    departmentProperty = text;
+
+}
+
+}

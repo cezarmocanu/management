b486af2dc876670e2fdb4bedbb4cc53257c2b298
malexa21 <aaliroalexandra@gmail.com>
2021-07-24 12:05:33 +0300

    [BE0007] added the empty constructor

---
+++ management-backend/src/main/java/com/home/managementbackend/entities/Team.java
@@ -0,0 +1,17 @@
+package com.home.managementbackend.entities;
+
+public class Team {
+
+public int numberProperty;
+public String textProperty;
+public String departmentProperty;
+
+public Team(){
+    
+    numberProperty = -1;
+    textProperty = "Default Team";
+    departmentProperty = "Default Department";
+
+}
+
+    }

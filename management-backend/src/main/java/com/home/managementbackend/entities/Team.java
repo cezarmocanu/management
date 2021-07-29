99c0458eb89b0ff4cced4d245e88a33338096984
malexa21 <aaliroalexandra@gmail.com>
2021-07-29 10:51:45 +0300

    [BE0005] fixed comments

--- management-backend/src/main/java/com/home/managementbackend/entities/Team.java
+++ management-backend/src/main/java/com/home/managementbackend/entities/Team.java
@@ -2,8 +2,15 @@ package com.home.managementbackend.entities;
 
 public class Team {
 
-public int numberProperty;
-public String textProperty;
-public String departmentProperty;
+public int number;
+public String name;
+public String department; 
 
-    }
+public Team(int number, String name,String department) {
+    this.number = number;
+    this.name = name;
+    this.department = department;
+
+}
+
+}

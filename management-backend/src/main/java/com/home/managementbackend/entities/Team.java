fb90ffbf31bdfa651a09e969f6e2d79a806826cd
cezarmocanu <cezarmocanu2014@gmail.com>
2021-07-29 11:03:43 +0300

    [BE0007] Modified constructor and rebased

--- management-backend/src/main/java/com/home/managementbackend/entities/Team.java
+++ management-backend/src/main/java/com/home/managementbackend/entities/Team.java
@@ -2,15 +2,19 @@ package com.home.managementbackend.entities;
 
 public class Team {
 
-public int number;
-public String name;
-public String department; 
+    public int id;
+    public String name;
+    public String department; 
 
-public Team(int number, String name,String department) {
-    this.number = number;
-    this.name = name;
-    this.department = department;
+    public Team() {
+        id = -1;
+        name = "Default Team";
+        department = "Default Department";
+    }
 
-}
-
+    public Team(int id, String name,String department) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+    }
 }
